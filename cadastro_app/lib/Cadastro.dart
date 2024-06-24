3c7045e82b0e108fa6141a7a50efe58ed890d6a8
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-06-23 21:37:35 -0300

    cadastro: ok

--- cadastro_app/lib/Cadastro.dart
+++ cadastro_app/lib/Cadastro.dart
@@ -1,5 +1,25 @@
-Class Cadastro {
+import 'package:flutter/material.dart';
+
+class Cadastro {
   int? id;
   String texto;
   int numerico;
+
+  Cadastro({this.id, required this.texto, required this.numerico});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'texto': texto,
+      'numerico': numerico,
+    };
+  }
+
+  factory Cadastro.fromMap(Map<String, dynamic> map){
+    return Cadastro(
+      id: map['id'],
+      texto: map['texto'],
+      numerico: map['numerico'],
+    );
+  }
 }
